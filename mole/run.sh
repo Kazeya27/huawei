ac1fa347ab0a9b6358859e5efb8c1daedb3e162b
wangyongyao <837826068@qq.com>
2025-04-08 21:06:29 +0800

    a

--- mole/run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-python script_nn_largest.py --method_name rpm_0006 --gpu_id 0 --n_worker 8 --data_name ca_his_2019_agg_0000
-
-python script_nn_largest.py --method_name rpm_0006 --gpu_id 0 --n_worker 8 --data_name gla_his_2019_agg_0000
-
-python script_nn_largest.py --method_name rpm_0006 --gpu_id 0 --n_worker 8 --data_name gba_his_2019_agg_0000
-
-python script_nn_largest.py --method_name rpm_0006 --gpu_id 0 --n_worker 8 --data_name sd_his_2019_agg_0000
